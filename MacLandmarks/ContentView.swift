7eda7ca7b49885772b653dce90f5f00458bbede6
Rahmanism <s.mostafa.rahmani@gmail.com>
2022-01-16 19:23:30 +0330

    Initial Commit

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Mostafa Rahmani on 1400/26/10 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Mostafa Rahmani on 1400/26/10 AP.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
